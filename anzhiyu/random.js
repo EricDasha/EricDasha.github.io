55457ce159668dd53dd9cac7622d29daa1680d62
EricDasha <146364373+EricDasha@users.noreply.github.com>
2024-10-05 12:41:17 +0800

    Site updated: 2024-10-05 12:41:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/06/Steamtools/","2024/04/20/一篇没用的文章/"];function toRandomPost(){
+var posts=["2024/09/20/部分游戏的伪局域网联机/","2024/04/20/一篇没用的文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
