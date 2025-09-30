b106609d745162b386eb5717d13271a2c5070301
EricDasha <146364373+EricDasha@users.noreply.github.com>
2025-09-30 15:10:42 +0800

    Site updated: 2025-09-30 15:10:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["_post/flash档案馆-软件推荐/","_post/keyboard/","_post/localsend-软件推荐/","_post/minecraft-pe/","_post/powershell安装appx/","_post/一篇没用的文章/","_post/意大利面拌42号混凝土/","_post/截图软件-软件推荐/","_post/手残联盟十周年/","_post/瞬译-软件推荐/","_post/赛博超能力/","_post/部分游戏的伪局域网联机/"];function toRandomPost(){
+var posts=["_post/flash档案馆-软件推荐/","_post/keyboard/","_post/minecraft-pe/","_post/localsend-软件推荐/","_post/powershell安装appx/","_post/一篇没用的文章/","_post/意大利面拌42号混凝土/","_post/截图软件-软件推荐/","_post/手残联盟十周年/","_post/瞬译-软件推荐/","_post/赛博超能力/","_post/部分游戏的伪局域网联机/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
